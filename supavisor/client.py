3723dc477a6099f1e16b46bf93ff8adb52a87577
jamal <jsaied99@gmail.com>
2024-07-18 15:18:37 -0500

    adding additional endpoints

--- supavisor/client.py
+++ supavisor/client.py
@@ -1,20 +1,42 @@
-from typing import Any
+from contextlib import asynccontextmanager
+from typing import Any, override
 
 import aiohttp
 import orjson
-from pydantic import BaseModel, Field, HttpUrl, SecretStr
+from pydantic import BaseModel, Field, HttpUrl, PrivateAttr, SecretStr
 
 from supavisor.types import AddTenantParams
 
 
 class Supavisor(BaseModel):
+    __headers: dict[str, str] = PrivateAttr(default_factory=dict)
     supavisor_url: HttpUrl = Field(
         description="Supavisor base url",
     )
     supavisor_api_key: SecretStr = Field(
         description="Supavisor API key",
     )
+    
+    @override
+    def __init__(self, supavisor_url: HttpUrl, supavisor_api_key: SecretStr):
+        super().__init__(supavisor_url=supavisor_url, supavisor_api_key=supavisor_api_key)
+        self.__headers = {
+            "Authorization": f"Bearer {self.supavisor_api_key.get_secret_value()}",
+            "Content-Type": "application/json",
+        }
 
+    @asynccontextmanager
+    async def session(self):
+        session = aiohttp.ClientSession(
+            timeout=aiohttp.ClientTimeout(total=90),
+            json_serialize=orjson.dumps,  # type: ignore[arg-type]
+        )
+        
+        try:
+            yield session
+        finally:
+            await session.close()
+            
     async def add_tenant(self, tenant: str, params: AddTenantParams) -> dict[str, Any]:
         """
         Endpoint to add a tenant
@@ -48,22 +70,15 @@ class Supavisor(BaseModel):
         """
 
         url = f"{self.supavisor_url.unicode_string()}/api/tenants/{tenant}"
-        headers: dict[str, str] = {
-            "Authorization": f"Bearer {self.supavisor_api_key.get_secret_value()}",
-            "Content-Type": "application/json",
-        }
-        async with aiohttp.ClientSession(
-            timeout=aiohttp.ClientTimeout(total=90),
-            json_serialize=orjson.dumps,  # type: ignore[arg-type]
-        ) as session:
+        async with self.session() as session:
             async with session.put(
-                url, headers=headers, data=params.model_dump_json()
+                url, headers=self.__headers, data=params.model_dump_json()
             ) as response:
                 res: dict[str, Any] = await response.json(
                     content_type=None, loads=orjson.loads
                 )
 
-                if response.status != 200:
+                if response.status >= 400:
                     raise Exception(response.status)
 
                 data = res.get("data")
@@ -72,3 +87,84 @@ class Supavisor(BaseModel):
                     raise Exception("Data is None")
 
                 return data
+
+    async def delete_tenant(self, tenant: str) -> bool:
+        """
+        Endpoint to delete a tenant
+        Example:
+
+        ```python
+        from supavisor.client import Supavisor
+        from pydantic import SecretStr
+
+        >>> supavisor = Supavisor(
+        ...     supavisor_url="http://localhost:4000",
+        ...     supavisor_api_key=SecretStr("secret"),
+        ... )
+        >>> tenant = "tenant1"
+
+        >>> await supavisor.delete_tenant(tenant)
+        True
+        """
+        url = f"{self.supavisor_url.unicode_string()}/api/tenants/{tenant}"
+        async with self.session() as session:
+            async with session.delete(url, headers=self.__headers) as response:
+                return response.status == 204
+            
+            
+    async def get_tenant(self, tenant: str) -> dict[str, Any]:
+        """
+        Endpoint to get a tenant
+        Example:
+
+        ```python
+        from supavisor.client import Supavisor
+        from pydantic import SecretStr
+
+        >>> supavisor = Supavisor(
+        ...     supavisor_url="http://localhost:4000",
+        ...     supavisor_api_key=SecretStr("secret"),
+        ... )
+        >>> tenant = "tenant1"
+
+        >>> await supavisor.get_tenant(tenant)
+        """
+        url = f"{self.supavisor_url.unicode_string()}/api/tenants/{tenant}"
+        async with self.session() as session:
+            async with session.get(url, headers=self.__headers) as response:
+                res: dict[str, Any] = await response.json(
+                    content_type=None, loads=orjson.loads
+                )
+
+                if response.status >= 400:
+                    raise Exception(response.status)
+
+                data = res.get("data")
+
+                if data is None:
+                    raise Exception("Data is None")
+
+                return data
+            
+    async def terminate_tenant(self, tenant: str) -> bool:
+        """
+        Endpoint to terminate a tenant
+        Example:
+
+        ```python
+        from supavisor.client import Supavisor
+        from pydantic import SecretStr
+
+        >>> supavisor = Supavisor(
+        ...     supavisor_url="http://localhost:4000",
+        ...     supavisor_api_key=SecretStr("secret"),
+        ... )
+        >>> tenant = "tenant1"
+
+        >>> await supavisor.terminate_tenant(tenant)
+        True
+        """
+        url = f"{self.supavisor_url.unicode_string()}/api/tenants/{tenant}/terminate"
+        async with self.session() as session:
+            async with session.get(url, headers=self.__headers) as response:
+                return response.status == 204
